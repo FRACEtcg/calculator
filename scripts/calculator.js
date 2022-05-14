82daf1048b92d611bdf11e0cd6b4e6dd9daf7d63
FRACEtcg <eugene.opalihin@gmail.com>
2022-05-14 08:46:13 +0300

    1 operator working version

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,13 +1,37 @@
 let buttons = document.getElementsByClassName('button');
-let screen = document.querySelector('.screen');
+let screentop = document.querySelector('.screentop');
+let screenbot = document.querySelector('.screenbot');
+let equal = document.querySelector('.equal');
+let result = 0; //new
 [...buttons].forEach((element) => {
 	element.addEventListener('click', () => {
 		func(element);
 	});
 });
+equal.addEventListener('click', () => {
+  calculate();
+  screenbot.textContent = '';
+});
 let func = function (element) {
-  if (screen.textContent === '0') {
-    screen.textContent = '';
+  if (screenbot.textContent === '0') {
+    screenbot.textContent = '';
   }
-	screen.textContent += element.textContent;
-};
+	screenbot.textContent += element.textContent;
+};//new
+let calculate = function () {
+  let myregex = /[-+×÷]/;
+  let operator = screenbot.textContent.match(myregex);
+  let calcarray = screenbot.textContent.split(myregex);// split with regex for any operator, then split by the operator, then if/else with different operators
+  // to perform calcarr[0] +(or ayn other operator) calcarr[2]
+  // if operator is clicked instead of an 'equal sign' use this --> calcstring.slice(0, -1); <-- before
+  if (operator[0] === '×') {
+    result = parseInt(calcarray[0]) * parseInt(calcarray[1]);
+  } else if (operator[0] === '÷') {
+    result = (parseInt(calcarray[0]) / parseInt(calcarray[1])).toFixed(5);
+  } else if (operator[0] === '+') {
+    result = parseInt(calcarray[0]) + parseInt(calcarray[1]);
+  } else { 
+    result = parseInt(calcarray[0]) - parseInt(calcarray[1]);
+  }
+  screentop.textContent = result;
+}
