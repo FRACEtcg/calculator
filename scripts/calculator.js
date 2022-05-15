e10f2dcea71ba57da444b6cf9f9d277140fccfa1
FRACEtcg <eugene.opalihin@gmail.com>
2022-05-15 07:32:44 +0300

    reworked the whole logic in js all operations working, decimal points on point (no pun intended), Need to add: -clear and delete buttons ramp up the design perhaps keyboard input

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -3,70 +3,87 @@ let operators = document.getElementsByClassName('operator');
 let screentop = document.querySelector('.screentop');
 let screenbot = document.querySelector('.screenbot');
 let equal = document.querySelector('.equal');
-let result = 0; //new
-let total = 0; //
+screenbot.textContent = '';
 firstoperand = '';
 secondoperand = '';
 let operator = '';
+let result = ''; //new
 [...buttons].forEach((element) => {
 	element.addEventListener('click', () => {
-		func(element);
-	})});
+		appendchar(element);
+	});
+}); //buttons ready
 [...operators].forEach((element) => {
 	element.addEventListener('click', () => {
-		func(element);
-    if (screentop.textContent.toString() === '') {screenbot.textContent = '';}
-    if (screentop.textContent.toString() != '' && operator != '') {
-      firstoperand = screenbot.textContent.toString().slice(1, -1);
-      calculate();
-    }
-    if (operator != '') {operator = element.textContent.toString();}
-    if (firstoperand != '') {
-
+		if (result === '') {
+			if (screenbot.textContent.length > 0) {
+				if (firstoperand === '') {
+					firstoperand = screenbot.textContent;
+					operator = element.textContent;
+					appendchar(element);
+				} else {
+					if (screenbot.textContent.length > firstoperand.length + 1) {
+						secondoperand = screenbot.textContent.slice(
+							firstoperand.length + 1
+						);
+						calculate();
+						operator = element.textContent;
+						screenbot.textContent = '';
+						appendchar(element);
+					}
+				}
+			}
 		} else {
-      firstoperand = screenbot.textContent.toString().slice(0, -1);
-			calculate();
+			if (screenbot.textContent.length > 1) {
+				secondoperand = screenbot.textContent.slice(1);
+				calculate();
+				operator = element.textContent;
+				screenbot.textContent = '';
+				appendchar(element);
+			} else if (screenbot.textContent === '') {
+				appendchar(element);
+				operator = element.textContent;
+			}
 		}
 	});
 });
 equal.addEventListener('click', () => {
-  calculate();
-  screenbot.textContent = '';
+	if (result === '') {
+		secondoperand = screenbot.textContent.slice(firstoperand.length + 1);
+		operator = screenbot.textContent.match(/[-+÷×]/).toString();
+		calculate();
+		screenbot.textContent = '';
+		operator = '';
+	} else {
+		if (screenbot.textContent.length > 1) {
+			secondoperand = screenbot.textContent.slice(1);
+			operator = screenbot.textContent.match(/[-+÷×]/).toString();
+			calculate();
+			screenbot.textContent = '';
+			operator = '';
+		}
+	}
 });
-let func = function (element) {
-  if (screenbot.textContent === '0') {
-    screenbot.textContent = '';
-  }
+let appendchar = function (element) {
+	//appendchar ready
+	if (screenbot.textContent === '0') {
+		screenbot.textContent = '';
+	}
 	screenbot.textContent += element.textContent;
-};//new
-let calculate = function () {
-  let myregex = /[-+×÷]/;
-  if (screentop.textContent != '' && operator != '') {
-    secondoperand = firstoperand;
-    parseInt(screentop.textContent)
-    if (operator[0] === '×') {
-    result = (parseInt(screentop.textContent) * parseInt(secondoperand)).toFixed(5);
-    } else if (operator[0] === '÷') {
-    result = (parseInt(screentop.textContent) / parseInt(secondoperand)).toFixed(5);
-    } else if (operator[0] === '+') {
-    result = (parseInt(screentop.textContent) + parseInt(secondoperand)).toFixed(5);
-    } else { 
-    result = (parseInt(screentop.textContent) - parseInt(secondoperand)).toFixed(5);
-    }
-  } else {
-    let calcarray = screenbot.textContent.split(myregex);// split with regex for any operator, then split by the operator, then if/else with different operators
-    // to perform calcarr[0] +(or ayn other operator) calcarr[2]
-    // if operator is clicked instead of an 'equal sign' use this --> calcstring.slice(0, -1); <-- before
-    if (operator === '×') {
-    result = (parseInt(calcarray[0]) * parseInt(calcarray[1])).toFixed(5);
-    } else if (operator === '÷') {
-    result = (parseInt(calcarray[0]) / parseInt(calcarray[1])).toFixed(5);
-    } else if (operator === '+') {
-    result = (parseInt(calcarray[0]) + parseInt(calcarray[1])).toFixed(5);
-    } else { 
-    result = (parseInt(calcarray[0]) - parseInt(calcarray[1])).toFixed(5);
-    }}
-  operator = '';
-  firstoperand ='';
-  screentop.textContent = result;
-}
+}; //new
+let calculate = async function () {
+	//calc ready
+	if (operator === '×') {
+		result = parseFloat((parseFloat(firstoperand) * parseFloat(secondoperand)).toFixed(5));
+	} else if (operator === '÷') {
+		result = parseFloat((parseFloat(firstoperand) / parseFloat(secondoperand)).toFixed(5));
+	} else if (operator === '+') {
+		result = parseFloat((parseFloat(firstoperand) + parseFloat(secondoperand)).toFixed(5));
+	} else {
+		result = parseFloat((parseFloat(firstoperand) - parseFloat(secondoperand)).toFixed(5));
+	}
+	if (isNaN(result)) {
+		firstoperand = firstoperand;
+	} else firstoperand = result;
+	screentop.textContent = firstoperand;
+};
